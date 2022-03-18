df33fc025a4c65a6257046d26841b9bb4e6693ce
kenta0202 <naiken0202@gmail.com>
2022-03-18 19:36:21 +0900

    a

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import "../../styles/dist.css";
-import type { AppProps } from "next/app";
-import { useEffect } from "react";
+import '../../styles/dist.css';
+import type { AppProps } from 'next/app';
+import { useEffect } from 'react';
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
