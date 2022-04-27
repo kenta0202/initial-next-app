1a14791e3318e19aed885a27d972f179ff20384d
kenta0202 <naiken0202@gmail.com>
2022-04-27 18:02:02 +0900

    src/@typesフォルダの作成

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,11 +2,15 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import userReducer from "../features/userSlice";
+// import { composeWithDevTools } from "redux-devtools-extension";
+
+// const composeEnhancers = composeWithDevTools({});
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
   },
+  /*  enhancers: composeEnhancers, */
 });
 
 export type AppDispatch = typeof store.dispatch;
