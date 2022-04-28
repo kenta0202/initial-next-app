51c2d549227c1cfc3af640a8bac0d8ec97c84db1
kenta0202 <naiken0202@gmail.com>
2022-04-28 14:37:48 +0900

    LayoutのHeader,Footerのパスを修正

--- src/components/general/Layout.tsx
+++ src/components/general/Layout.tsx
@@ -1,5 +1,5 @@
-import Header from "./header";
-import Footer from "./footer";
+import Footer from "./Footer";
+import Header from "./Header";
 
 export default function Layout({ children }) {
   return (
