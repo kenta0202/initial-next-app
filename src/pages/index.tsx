3759887da4a174e240144cc0c124a2dad15a1c04
kenta0202 <naiken0202@gmail.com>
2022-05-13 19:27:50 +0900

    Homeのテキストの色を黒く

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import Count from "pages/sample/redux/Count";
 function IndexPage() {
   return (
     <>
-      <h1 className=" text-xl text-red-300">func D 👋 </h1>
+      <h1 className=" text-xl">func D 👋 </h1>
       <Count />
     </>
   );
