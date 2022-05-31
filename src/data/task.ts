2b7564f3eee15262c6cebf8b84f884ada87eebb7
kenta0202 <naiken0202@gmail.com>
2022-05-31 22:53:30 +0900

    a

--- src/data/task.ts
+++ src/data/task.ts
@@ -4,24 +4,31 @@ import { TTag, TTask } from "interface/todo/tasks"
 export const TaskList: TTask[] = [
   {
     id: "1",
-    title: "Implement feature",
+    title: "1000足歩く",
     created_at: "2021-05-10",
     updated_at: "2021-05-10",
-    tag_name: "tag1",
+    tag_name: "健康",
   },
   {
     id: "2",
-    title: "Unit Test➀",
+    title: "歯磨きをする",
     created_at: "2021-05-13",
     updated_at: "2021-05-13",
-    tag_name: "tag2",
+    tag_name: "清潔",
   },
   {
     id: "3",
-    title: "Unit Test➁",
+    title: "食べ過ぎない",
     created_at: "2021-05-13",
     updated_at: "2021-05-13",
-    tag_name: "tag3",
+    tag_name: "健康",
+  },
+  {
+    id: "3",
+    title: "一日一時間勉強する",
+    created_at: "2021-05-13",
+    updated_at: "2021-05-13",
+    tag_name: "学習",
   },
 ]
 
